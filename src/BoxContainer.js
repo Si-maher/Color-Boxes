9bc4fd8efec2d4f4ec8dc3be68ac97935a193d1c
Simon Maher <simon.maher@integrify.io>
2019-06-07 16:21:20 +0300

    Created Box container, map method to make 18 different boxes

---
+++ src/BoxContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import Box from "./Box"
+class BoxContainer extends Component {
+    static defaultProps = {
+        numBoxes = 18
+    }
+    render() {
+        const boxes = Array.from({ length: this.props.numBoxes }).map(
+            () => <Box />
+        )
+
+    }
+}
+export default BoxContainer
